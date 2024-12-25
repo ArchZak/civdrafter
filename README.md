7769bc10e4b9a5f974d8b2257ccb793014cd8d3f
Aarash Zakeri <izear21@gmail.com>
2024-12-25 01:45:16 -0600

    civdrafter created with features for listing all the leaders, drafting civs to players, and featuring help

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# civdrafter
-A really simple civilization six drafter for your discord server
+### civdrafter
+
+Civdrafter is a really simple discord bot I made for my friends and I so we could draft out civ leaders in a way that's really easy and intuitive
+
+## Prereqs
+
+You need to install the following things into your terminal in order to make sure you can run the bot properly. You'll need node and npm, make sure to check if you have them installed before trying to install the dependencies
+
+```bash
+node -v
+npm -v
+```
+
+# install dependencies
+
+```bash
+npm i
+npm init -y
+npm install discord.js
+```
+
+# enviornment variable
+
+The last thing you'll need to do is to create and enviornment and make sure you securely store your discord token inside of it. You can use the following command to create the env file in which you will paste your discord bot ID into
+
+```bash
+echo "DISCORD_TOKEN=your-bot-token" > .env
+```
+
+After, you will need to run some extra commands and you're all good to run the bot using node.js
+
+```bash
+npm install dotenv
+```
+
+
---
+++ civ.js
@@ -0,0 +1,144 @@
+const leaders = [ //all the civ6 leaders...
+    "america-bullmoose",
+    "america-lincoln",
+    "america-roughrider",
+    "arabia-sultan",
+    "arabia-vizier",
+    "australia",
+    "aztec",
+    "babylon",
+    "brazil",
+    "byzantine-basil",
+    "byzantine-theodora",
+    "canada",
+    "china-kublai",
+    "china-mandate",
+    "china-unifer",
+    "china-yongle",
+    "china-zetian",
+    "dutch",
+    "egypt-ramses",
+    "english-eleanor",
+    "english-empire",
+    "english-steam",
+    "ethiopia",
+    "french-blackqueen",
+    "french-eleanor",
+    "french-magnificence",
+    "gaul",
+    "georgia",
+    "germany-frederick",
+    "germany-ludwig",
+    "greek-gorgo",
+    "greek-pericles",
+    "grancolombia",
+    "hungary",
+    "inca",
+    "india-chandragupta",
+    "india-ghandi",
+    "indonesia",
+    "japan-hojo",
+    "japan-tokugawa",
+    "khmer",
+    "kongo-mvemba",
+    "kongo-nzinga",
+    "korea-sejong",
+    "korea-seondeok",
+    "macedon",
+    "mali-keita",
+    "mali-mansamusa",
+    "maori",
+    "mapuche",
+    "mayan",
+    "mongolia-genghis",
+    "mongolia-kublai",
+    "norway-konge",
+    "norway-varangian",
+    "nubia",
+    "persia-cyrus",
+    "persia-nadar",
+    "phoenicia",
+    "polish",
+    "portugal",
+    "poundmaker",
+    "ptolemaic-cleopatra",
+    "roman-trajan",
+    "rome-ceasar",
+    "russia",
+    "scotland",
+    "scythia",
+    "spain",
+    "sumeria",
+    "sweden",
+    "vietnam",
+    "zulu"
+];
+
+const Discord = require('discord.js'); 
+require('dotenv').config();
+
+const token = process.env.DISCORD_TOKEN;
+
+const Client = new Discord.Client({ //discord.js imports
+    intents: [
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.DirectMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.Guilds,
+    ], partials: [
+        Discord.Partials.Message,
+        Discord.Partials.Channel,
+        Discord.Partials.GuildMember,
+        Discord.Partials.User,
+        Discord.Partials.GuildScheduledEvent,
+        Discord.Partials.ThreadMember,
+    ]
+}); 
+
+Client.on('ready', (client) => console.log(client.user.tag + ' is now online'));
+
+Client.on('messageCreate', (message) => {  //scans every message typed in any channel the bot has access to
+    if (message.content === '?help') {
+        message.reply(
+            "Use `?draft X Y LeaderName1 LeaderName2 etc` to create a draft. "+ 
+            "X is the amount of players to draft for, and Y is the amount of civs to assign to each player. The leader names after are the bans, you can type any amount of bans." +
+            "\n\nUse `?listleaders` in order to see all the civ leaders that are available,"+
+            " and the names you need to copy and paste into the leader ban.");
+    } else if (message.content === "?listleaders") {
+        message.reply(leaders.map((leader, index) => `${index + 1}. ${leader}`).join("\n"));
+    }
+
+    const messageContents = (message.content).split(" ");
+
+    if (messageContents[0] === '?draft') {
+        const playerAmount = messageContents[1]; 
+        const civAmount = messageContents[2];
+
+        const bannedLeaders = messageContents.slice(3).map(ban => ban.toLowerCase());
+
+        const availableLeaders = leaders.filter(leader => //makes sure dupe leaders arent handed out
+            !bannedLeaders.includes(leader.toLowerCase())
+        );
+
+        if (availableLeaders.length < playerAmount * civAmount) {
+            return message.reply("not enough leaders available after bans to complete draft");
+        }
+
+        const draftResults = [];
+        for (let i = 0; i < playerAmount; i++) {
+            const playerDraft = []; //assigns civs to each player at random
+            for (let j = 0; j < civAmount; j++) {
+                const randomNumber = Math.floor(Math.random() * availableLeaders.length);
+                playerDraft.push(availableLeaders[randomNumber]);
+                availableLeaders.splice(randomNumber, 1); 
+            }
+            draftResults.push(`Player ${i + 1}: ${playerDraft.join(", ")}`); 
+        }
+
+        message.reply(draftResults.join("\n"));
+    }
+    }
+); 
+
+Client.login(token); 
